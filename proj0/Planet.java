859365483b48ee22cb821dd02f7e9170b4438d2f
colaelevator <liangxu114514@mail.ustc.edu.cn>
2025-08-10 17:31:51 +0800

    Planet

---
+++ proj0/Planet.java
@@ -0,0 +1,33 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    String imgFileName;
+
+    public Planet(double xP,double yP,double xV,double yV,double m,String img) {
+        xxPos=xP;
+        yyPos=yP;
+        xxVel=xV;
+        yyVel=yV;
+        mass=m;
+        imgFileName=img;
+    }
+    public Planet(Planet p) {
+        xxPos=p.xxPos;
+        yyPos=p.yyPos;
+        xxVel=p.xxVel;
+        yyVel=p.yyVel;
+        mass=p.mass;
+        imgFileName=p.imgFileName;
+    }
+
+    public static double calcDistance(Planet p) {
+        double dx=this.xxPos-p.xxPos;
+        double dy=this.yyPos-p.yyPos;
+        return Math.pow()
+
+
+    }
+}
